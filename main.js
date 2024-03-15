199a2c0fb1570c201d53d5a784408636996c314c
mariaaa <108476641+mariiia1@users.noreply.github.com>
2024-03-15 12:00:52 -0300

    edit

--- main.js
+++ main.js
@@ -14,3 +14,9 @@ for(let i=0;i < botoes.length;i++){
  }
 
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo - tempoAtual;
